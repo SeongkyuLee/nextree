1041e2cead6d737acaa55035613b965643f7fc68
junksound <wom2277@naver.com>
2018-02-03 11:46:01 +0900

    delete_all

--- backend/nextree_server/bookmark/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from bookmark.models import Post
-
-class PostSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Post
-        fields = ('title','topic','url','description')
-
--- backend/nextree_server/bookmark/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-from bookmark.models import Post
-from bookmark.serializer import PostSerializer
-# Create your views here.
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
